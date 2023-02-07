5eec16b64218e543530c1d68d2e3926586834a09
saraPetrini <122064215+saraPetrini@users.noreply.github.com>
2023-02-07 17:28:49 +0100

    Moved function operations. Removed comments

--- Webapp.py
+++ Webapp.py
@@ -8,21 +8,30 @@ def get_df():
 	d = DatasetGFF3('Homo_sapiens.GRCh38.85.gff3')
 	return d.dataframe
 
+
+def operations():
+	dic = {'info': info,
+		'seqID': seqID,
+		'type_operation':type_operation,
+		'count_features': count_features,
+		'count_entries':count_entries,
+		'entire_chromosome': entire_chromosome,
+		'count_supercontigs': count_supercontigs,
+		'entries_ens_hav_enshav': entries_ens_hav_enshav,
+		'count_entries_ens_hav_enshav':count_entries_ens_hav_enshav,
+		'gene_names': gene_names}  
+	return dic
+
 webapp = Flask(__name__)
 
 @webapp.route('/')
 def homepage():
-	#df = get_df()
-	#return render_template('html_homepage.html', dataframe=df)
 	return render_template('html_homepage.html')
 	
 @webapp.route('/html_activeoperations.html')
 def active_operations():
 	d = Active().get_dic()
-	#dic = d.get_dic()
 	return render_template('html_activeoperations.html', dic = d)
-	
-'''possiamo usare la funzione redirect('/') o redirect('/html_activeoperations.html' se viene selezionata un'operazione non attiva')'''
 
 @webapp.route('/html_operation.html')
 def operation():
