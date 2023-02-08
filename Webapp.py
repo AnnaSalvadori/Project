3e39150a456dc075d33533c22288100973deaca7
AnnaSalvadori <124202886+AnnaSalvadori@users.noreply.github.com>
2023-02-08 13:45:49 +0100

    Active

--- Webapp.py
+++ Webapp.py
@@ -31,7 +31,7 @@ def homepage():
 	
 @webapp.route('/html_activeoperations.html')
 def active_operations():
-	d = Active()
+	d = active()
 	return render_template('html_activeoperations.html', dic = d)
 
 @webapp.route('/html_operation.html')
